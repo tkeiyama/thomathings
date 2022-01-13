3486a21af74a4e37d97ee02309fe3fe08bf0e269
tkeiyama <thomas18thx@gmail.com>
2022-01-13 17:05:47 +0900

    Change the cursor when hovering on the summary tag

--- things/things-that-i-note/Section.tsx
+++ things/things-that-i-note/Section.tsx
@@ -8,7 +8,7 @@ interface SectionProps {
 export function Section({ children, title }: SectionProps): ReactElement {
   return (
     <details className="mt-10">
-      <summary className="text-xl">{title}</summary>
+      <summary className="text-xl cursor-pointer">{title}</summary>
       <div className="p-4 rounded-b-lg border border-t-0 border-white">
         {children}
       </div>
