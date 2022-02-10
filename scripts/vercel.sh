c8491f0977cd3ebf44345f27afc7211833f7b48a
tkeiyama <thomas18thx@gmail.com>
2022-02-11 00:15:57 +0900

    Make the vercel's build work on every branch

--- scripts/vercel.sh
+++ scripts/vercel.sh
@@ -2,7 +2,7 @@
 
 echo "VERCEL_GIT_COMMIT_REF: $VERCEL_GIT_COMMIT_REF"
 
-if [[ "$VERCEL_GIT_COMMIT_REF" == *"renovate"* ]]; then
+if [[ "$VERCEL_GIT_COMMIT_REF" == "" ]]; then
   echo "🛑 - Build cancelled because of the ignored branch"
   exit 0;
 
