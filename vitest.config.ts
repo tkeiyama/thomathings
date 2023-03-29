ff7b102b9c0bbe4b702178afd626f6400b771417
tkeiyama <thomas18thx@gmail.com>
2023-03-29 23:44:28 +0900

    Move to pnpm from yarn

--- vitest.config.ts
+++ vitest.config.ts
@@ -6,7 +6,7 @@ export default defineConfig({
   test: {
     globals: true,
     environment: "jsdom",
-    setupFiles: "./vitest.setup.js",
+    setupFiles: "./vitest.setup.ts",
     coverage: {
       provider: "c8",
       reporter: ["html"],
