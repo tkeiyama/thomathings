ff4b95262d040efd6d146ec85ef26008bc2a51d7
tkeiyama <thomas18thx@gmail.com>
2024-09-13 17:53:41 +0000

    added the start script

--- vite.config.ts
+++ vite.config.ts
@@ -2,12 +2,9 @@ import { vitePlugin as remix } from "@remix-run/dev";
 import { defineConfig } from "vite";
 import tsconfigPaths from "vite-tsconfig-paths";
 import { remixDevTools } from "remix-development-tools";
-import {vercelPreset} from '@vercel/remix/vite'
 
 export default defineConfig({
-	plugins: [remixDevTools(), remix({
-		presets: [vercelPreset()]
-	}), tsconfigPaths()],
+	plugins: [remixDevTools(), remix(), tsconfigPaths()],
 	server: {
 		port: 50000,
 		host: "0.0.0.0",
